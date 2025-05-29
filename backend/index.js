b9286cf7cd0dbf6b1ee0644d5b165d558e1e3e98
am-prasad <amprasad18@gmail.com>
2025-05-29 17:55:59 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-import express from 'express'
+import express, {json} from 'express'
 import mongoose from 'mongoose';
+
+
 const app = express()
+app.use(json());
 mongoose.connect('mongodb://127.0.0.1:27017/')
   .then(() => console.log('Connected!')).catch(()=>{console.log(`not cconncected`)});
 
@@ -9,8 +12,11 @@ app.listen(3000,()=>{console.log(`Server is running`)})
 
 app.get('/', (req, res) => {
   res.send('Pras')
-})
 
+})
+app.post('/api/item',(req,res)=>{
+  console.log(req.body);
+  res.send(req.body);})
   
 
 // app.listen(3000)
