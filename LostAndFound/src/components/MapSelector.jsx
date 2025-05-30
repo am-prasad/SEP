93eb03426a6af06c506dde5253837b231d1fd657
am-prasad <amprasad18@gmail.com>
2025-05-30 20:23:36 +0530

    react_leaf_map added

--- LostAndFound/src/components/MapSelector.jsx
+++ LostAndFound/src/components/MapSelector.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents
+} from 'react-leaflet';
 import L from 'leaflet';
 import { Button } from '@/components/ui/button';
 import { toast } from 'sonner';
 import { campusLocations } from '@/data/mockData';
 
-// Fix default marker icon issue with Leaflet + Webpack/React
+// Fix default marker icon issue with Webpack/Vite and Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
-
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
@@ -17,6 +22,7 @@ L.Icon.Default.mergeOptions({
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
+// Component for listening to map click events
 const LocationSelector = ({ onLocationSelect }) => {
   useMapEvents({
     click(e) {
@@ -28,26 +34,25 @@ const LocationSelector = ({ onLocationSelect }) => {
       };
       onLocationSelect(newLocation);
       toast.info('Location selected', {
-        description: `Coordinates: ${lat.toFixed(6)}, ${lng.toFixed(6)}`,
+        description: `Coordinates: ${lat.toFixed(6)}, ${lng.toFixed(6)}`
       });
-    },
+    }
   });
-
   return null;
 };
 
+// Main MapSelector component
 const MapSelector = ({ onLocationSelect }) => {
   const [selectedLocation, setSelectedLocation] = useState(null);
 
-  // Select a predefined campus location
-  const selectCampusLocation = (location) => {
+  const handleSelect = (location) => {
     setSelectedLocation(location);
     onLocationSelect(location);
   };
 
   return (
     <div className="h-full flex flex-col">
-      <div className="p-2 border-b">
+      <div className="p-2 border-b bg-muted">
         <div className="flex flex-wrap gap-2">
           {campusLocations.map((location, index) => (
             <Button
@@ -55,7 +60,7 @@ const MapSelector = ({ onLocationSelect }) => {
               variant="outline"
               size="sm"
               className="text-xs"
-              onClick={() => selectCampusLocation(location)}
+              onClick={() => handleSelect(location)}
             >
               {location.description}
             </Button>
@@ -65,8 +70,8 @@ const MapSelector = ({ onLocationSelect }) => {
 
       <div className="flex-1 relative">
         <MapContainer
-          center={[40.1062, -88.2272]} // Default center
-          zoom={15}
+          center={[40.1062, -88.2272]} // Default center (UIUC example)
+          zoom={16}
           scrollWheelZoom={true}
           style={{ height: '100%', width: '100%' }}
         >
@@ -75,15 +80,8 @@ const MapSelector = ({ onLocationSelect }) => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {/* Allow user to click on map to select location */}
-          <LocationSelector
-            onLocationSelect={(loc) => {
-              setSelectedLocation(loc);
-              onLocationSelect(loc);
-            }}
-          />
+          <LocationSelector onLocationSelect={handleSelect} />
 
-          {/* Show marker for selected location */}
           {selectedLocation && (
             <Marker position={[selectedLocation.lat, selectedLocation.lng]}>
               <Popup>{selectedLocation.description}</Popup>
