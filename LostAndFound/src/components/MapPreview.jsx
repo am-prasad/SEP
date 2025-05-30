8751a3931ba4f7b612d24049a69ecd79278db7aa
am-prasad <amprasad18@gmail.com>
2025-05-30 17:09:28 +0530

    API secured

--- LostAndFound/src/components/MapPreview.jsx
+++ LostAndFound/src/components/MapPreview.jsx
@@ -9,7 +9,7 @@ borderRadius: '0.5rem',
 
 const MapPreview = ({ lat, lng }) => {
 const { isLoaded } = useLoadScript({
-googleMapsApiKey: 'AIzaSyBxWIWCGfP12Y0DKWq2adEXybF8fQgJ2JQ', // Replace with your actual key
+googleMapsApiKey: 'AIzaSyD31XQpnoc_pNDyZhCjjt8Ei6TTZ1xC_LI', // Replace with your actual key
 });
 
 if (!isLoaded) return <div>Loading Map...</div>;
